44c2d43695b94d23cda7fbdf4ea410e3ea3ce246
Martijn The <post@martijnthe.nl>
2012-04-09 20:49:48 +0200

    Added "pause" functionality to file player unit

--- YBAudioUnit/YBScheduledSoundPlayer.m
+++ YBAudioUnit/YBScheduledSoundPlayer.m
@@ -9,7 +9,9 @@
 #import "YBScheduledSoundPlayer.h"
 #import "YBAudioException.h"
 
-@implementation YBScheduledSoundPlayer
+@implementation YBScheduledSoundPlayer {
+    BOOL _hasStartTimeStamp;
+}
 
 - (AudioTimeStamp)currentPlayTime {
     AudioTimeStamp currentPlayTime;
@@ -27,10 +29,30 @@
     startTime.mFlags = kAudioTimeStampSampleTimeValid;
     startTime.mSampleTime = startSampleTime;
     YBAudioThrowIfErr(AudioUnitSetProperty(_auAudioUnit, kAudioUnitProperty_ScheduleStartTimeStamp, kAudioUnitScope_Global, 0, &startTime, sizeof(startTime)));
+    _hasStartTimeStamp = YES;
 }
 
 - (void)setStartTimeStamp:(AudioTimeStamp*)startTime {
     YBAudioThrowIfErr(AudioUnitSetProperty(_auAudioUnit, kAudioUnitProperty_ScheduleStartTimeStamp, kAudioUnitScope_Global, 0, &startTime, sizeof(startTime)));
+    _hasStartTimeStamp = YES;
 }
 
+- (BOOL)isPlaying {
+    AudioTimeStamp currentPlayTime;
+    UInt32 dataSize = sizeof(currentPlayTime);
+    YBAudioThrowIfErr(AudioUnitGetProperty(_auAudioUnit, kAudioUnitProperty_CurrentPlayTime, kAudioUnitScope_Global, 0, &currentPlayTime, &dataSize));
+    return (currentPlayTime.mSampleTime != -1.);
+}
+
+- (void)unschedule {
+    [self reset];
+}
+
+- (void)reset {
+    _hasStartTimeStamp = NO;
+    [super reset];
+}
+
+
+@synthesize hasStartTimeStamp = _hasStartTimeStamp;
 @end
